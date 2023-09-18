33054c364fed303dbebef08c3c771c6d7bff6b5e
Sarunyou Tee <swhan@sertiscorp.com>
2023-09-18 22:30:31 +0700

    feat: update keymap

--- keyboards/qwertykeys/qk65/hotswap/keymaps/sarunyou.whang/keymap.c
+++ keyboards/qwertykeys/qk65/hotswap/keymaps/sarunyou.whang/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(1, KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
         MT(MOD_LCTL,KC_ESC), HOME_A,   HOME_S,  HOME_D,  HOME_F,  KC_G,    KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_SCLN,  KC_QUOT,          KC_ENT,  KC_PGDN,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   MO(1),
-        KC_LCTL, KC_LALT, MO(1),                             KC_SPC,                    KC_RALT, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LALT, MO(1),                             KC_SPC,                    KC_BSPC, KC_DEL,          KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [1] = LAYOUT_65_ansi_blocker(
