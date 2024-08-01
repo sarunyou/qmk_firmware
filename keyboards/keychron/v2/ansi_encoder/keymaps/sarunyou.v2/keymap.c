f69802cb82b21713d63ba04b811f2171409ea5b2
Sarunyou Tee <swhan@sertiscorp.com>
2024-08-01 12:47:39 +0700

    feat: add symbol layer on keychron

--- keyboards/keychron/v2/ansi_encoder/keymaps/sarunyou.v2/keymap.c
+++ keyboards/keychron/v2/ansi_encoder/keymaps/sarunyou.v2/keymap.c
@@ -27,8 +27,6 @@ enum layers{
     _FN3
 };
 
-#define KC_TASK LGUI(KC_TAB)
-#define KC_FLXP LGUI(KC_E)
 
 // Left-hand home row mods
 #define HOME_A LCTL_T(KC_A)
@@ -42,13 +40,25 @@ enum layers{
 #define HOME_L LALT_T(KC_L)
 #define HOME_SCLN RCTL_T(KC_SCLN)
 
+// Left-hand home row mods on symbol layer
+#define HOME_EXLM_SYMBOL KC_EXLM
+#define HOME_MINS_SYMBOL KC_MINS
+#define HOME_PLUS_SYMBOL KC_PLUS
+#define HOME_EQL_SYMBOL KC_EQL
+
+// Right-hand home row mods on symbol layer
+#define HOME_COLON_SYMBOL KC_COLN
+#define HOME_RPRN_SYMBOL KC_RPRN
+#define HOME_LPRN_SYMBOL KC_LPRN
+#define HOME_QUES_SYMBOL KC_QUES
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_67(
         KC_GRV,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
         KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
         MT(MOD_LCTL,KC_ESC), HOME_A,   HOME_S,  HOME_D,  HOME_F,  KC_G,  KC_H,    HOME_J,  HOME_K,  HOME_L,   HOME_SCLN,KC_QUOT,  KC_ENT,           KC_HOME,
         KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            KC_RSFT, KC_UP,
-        KC_LCTL, KC_LOPT, MO(_FN1),                            KC_SPC,                             KC_RCMD, MO(_FN1), MO(_FN3), KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LOPT, MO(_FN1),                            KC_SPC,                             MO(_FN2), MO(_FN1), MO(_FN3), KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_BASE] = LAYOUT_ansi_67(
         KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
@@ -58,17 +68,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LGUI,  KC_LALT,                             KC_SPC,                             KC_RALT,  MO(_FN2), MO(_FN3), KC_LEFT, KC_DOWN, KC_RGHT),
 
     [_FN1] = LAYOUT_ansi_67(
-        KC_GRV,  KC_BRID,  KC_BRIU,  KC_MCTL, KC_LPAD, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,          RGB_TOG,
-        RGB_TOG, RGB_MOD,  RGB_VAI,  RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______,  _______,  _______,  _______,          _______,
-        _______, RGB_RMOD, RGB_VAD,  RGB_HUD, RGB_SAD, RGB_SPD, KC_CAPS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,  _______,            _______,          _______,
+        KC_GRV,  KC_BRID,  KC_BRIU,  KC_MCTL, KC_LPAD, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,          KC_VOLU,
+        _______, KC_1,        KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, _______,          KC_VOLD,
+        _______, RGB_RMOD, RGB_VAD,  RGB_HUD, RGB_SAD, RGB_SPD, KC_CAPS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,  _______,            _______,          KC_MUTE,
         _______,           _______,  _______, _______, _______, _______, NK_TOGG, _______, _______, _______,  _______,            _______, _______,
         _______, _______,  _______,                             _______,                            _______,  _______,  _______,  _______, _______, _______),
 
     [_FN2] = LAYOUT_ansi_67(
         KC_GRV,  KC_BRID,  KC_BRIU,  KC_TASK, KC_FLXP, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,          RGB_TOG,
-        RGB_TOG, RGB_MOD,  RGB_VAI,  RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______,  _______,  _______,  _______,          _______,
-        _______, RGB_RMOD, RGB_VAD,  RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______,  _______,            _______,          _______,
-        _______,           _______,  _______, _______, _______, _______, NK_TOGG, _______, _______, _______,  _______,            _______, _______,
+        _______, KC_QUOT, KC_LABK, KC_RABK, KC_DQUO, KC_GRV,  KC_AMPR, KC_SCLN, KC_LBRC, KC_RBRC, KC_PERC, _______, _______, _______, _______,
+        _______, HOME_EXLM_SYMBOL, HOME_MINS_SYMBOL, HOME_PLUS_SYMBOL, HOME_EQL_SYMBOL,  KC_HASH, KC_PIPE, HOME_COLON_SYMBOL, HOME_LPRN_SYMBOL, HOME_RPRN_SYMBOL, HOME_QUES_SYMBOL, _______,          _______, _______,
+        _______,          KC_CIRC, KC_SLSH, KC_ASTR, KC_BSLS, KC_UNDS,  KC_TILD, KC_DLR,  KC_LCBR, KC_RCBR, KC_AT,  _______, _______,
         _______, _______,  _______,                             _______,                            _______,  _______,  _______,  _______, _______, _______),
 
     [_FN3] = LAYOUT_ansi_67(
